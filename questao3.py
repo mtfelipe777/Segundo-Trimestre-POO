d1e9fe80ae00492405a99253ada5cbbb1ee02ca2
Matheus <matheuslabanca0@gmail.com>
2026-06-17 17:21:36 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -3,39 +3,38 @@ class Carteira:
         self.moeda = moeda
         self.saldo = saldo
     def __add__(self, valor_yuan):
-        self.valor_yuan = valor_yuan
-        self.saldo += self.valor_yuan
-        print(self.saldo)
+        self.saldo += valor_yuan
+        print("Saldo atual:", self.saldo)
     def __sub__(self, valor_yuan):
-        self.saldo -= self.valor_yuan
-        print(self.saldo)        
+        self.saldo -= valor_yuan
+        print("Saldo atual:", self.saldo)
+
+
 print("****Opções de moeda:****")
-print("1.USD;")
-print("2.BRL.")
-opcao = int(input("Informe a opção de moeda"))
+print("1. USD")
+print("2. BRL")
+opcao = int(input("Informe a opção de moeda: "))
+
 if opcao == 1:
-    carteira_usd = Carteira("USD", float(input("Informe seu saldo: "))*0.14)
+    carteira = Carteira("USD", float(input("Informe seu saldo: ")) * 0.14)
 elif opcao == 2:
-    carteira_brl = Carteira("BRL", float(input("Informe seu saldo: "))*0.85)
+    carteira = Carteira("BRL", float(input("Informe seu saldo: ")) * 0.85)
 else:
     print("Viajou")
+    exit()
+
 print("****Operadores****")
-print("1.Soma;")
-print("2.Subtração.")
+print("1. Soma")
+print("2. Subtração")
+print("3. Sair")
+
 while True:
-    if opcao == 1:
-        operadores = int(input("Informe a operação: "))
-        if operadores == 1:
-            carteira_usd.__add__()
-        elif operadores == 2:
-            carteira_usd.__sub__()
-        else:
-            break
-    elif opcao == 2:
-        operadores = int(input("Informe a operação: "))
-        if operadores == 1:
-            carteira_brl.__add__()
-        elif operadores == 2:
-            carteira_brl.__sub__()
-        else:
-            break
+    operadores = int(input("Informe a operação: "))
+    if operadores == 1:
+        valor = float(input("Informe o valor: "))
+        carteira.__add__(valor)
+    elif operadores == 2:
+        valor = float(input("Informe o valor: "))
+        carteira.__sub__(valor)
+    else:
+        break
