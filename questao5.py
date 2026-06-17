1fed1d067dbf139136c30aba220ac590fb5a39e5
Matheus <matheuslabanca0@gmail.com>
2026-06-17 17:21:32 -0300

    Create questao5.py

---
+++ questao5.py
@@ -0,0 +1,7 @@
+class Onibus:
+    def __init__(self, placa, nome_motorista, assentos):
+        self.placa = placa
+        self.nome_motorista = nome_motorista
+        self.assentos = []
+    def __len__(self):
+        
